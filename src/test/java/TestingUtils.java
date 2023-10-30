3d98faeb56f5fefb2d29d79e784eaa7d724d5418
MinhTranTuan12 <minhttse172842@fpt.edu.vn>
2023-10-30 09:16:17 +0700

    Update TC03, TC04 and TC05

--- src/test/java/TestingUtils.java
+++ src/test/java/TestingUtils.java
@@ -1,16 +1,15 @@
-import org.junit.jupiter.api.Assertions;
+import POM.RegisterPage;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.nio.file.Files;
-
+import POM.*;
 public class TestingUtils {
     //Fields
     private static final String imagesPath = "src\\main\\resources";
@@ -85,4 +84,268 @@ public class TestingUtils {
         Assertions.assertEquals(phonePrice, 100);
         driver.quit();
     }
+
+    @Test
+    public void TC03() {
+        int shoppingCartCnt = 0;
+        StringBuffer verificationError = new StringBuffer();
+        //init web driver session
+        System.setProperty("webdriver.chrome.driver", "D:\\Tools\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://live.techpanda.org/");
+        try {
+            //Step 2. Click on Mobile Menu
+            driver.findElement(By.linkText("MOBILE")).click();
+
+            Thread.sleep(2000);
+
+            //Click Add To Cart
+            driver.findElement(By.xpath(".//*//li[2]//div[1]//div[3]//button[1]//span[1]//span[1]")).click();
+
+
+            Thread.sleep(2000);
+
+            //Change quantity element to 1000
+            driver.findElement(By.xpath(".//*[@id='shopping-cart-table']/tbody/tr/td[4]/input")).clear();
+            driver.findElement(By.xpath(".//*[@id='shopping-cart-table']/tbody/tr/td[4]/input")).sendKeys("1000");
+            driver.findElement(By.xpath(".//*[@id='shopping-cart-table']/tbody/tr/td[4]/button")).click();
+
+
+            Thread.sleep(2000);
+
+            //Step 5. Verify the error message
+            String reqQty = driver.findElement(By.xpath(".//*[@id='shopping-cart-table']/tbody/tr/td[2]/p")).getText();
+            String msgQty = ("* The maximum quantity allowed for purchase is 500.");
+            try {
+                Assert.assertEquals(reqQty, msgQty);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //Click on empty cart link
+            driver.findElement(By.xpath(".//*[@id='empty_cart_button']")).click();
+
+
+            Thread.sleep(3000);
+
+            // Verify cart is empty
+            String noItemsStg = ("You have no items in your shopping cart.");
+            String noItemsMsg = driver.findElement(By.xpath(".//*[@id='top']/body/div[1]/div/div[2]/div/div/div[2]/p[1]")).getText();
+            System.out.println("You have no items msg = " + noItemsMsg);
+
+            try {
+                Assert.assertEquals(noItemsStg, noItemsMsg);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Quit
+        driver.quit();
+    }
+
+    @Test
+    public void TC04() {
+        int scc = 0;
+
+        StringBuffer verificationError = new StringBuffer();
+        //init web driver session
+        System.setProperty("webdriver.chrome.driver", "D:\\Tools\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        try {
+            //Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+
+            //Click on Mobile menu
+            driver.findElement(By.linkText("MOBILE")).click();
+
+            Thread.sleep(2000);
+
+            //Click to compare 2 phones
+            driver.findElement(By.xpath(".//*//li[3]//div[1]//div[3]//ul[1]//li[2]//a[1]")).click();
+            String Mobile1 = driver.findElement(By.xpath("//h2/a[@title='IPhone']")).getText();
+            System.out.println("Mobile1 = " + Mobile1);
+            Thread.sleep(2000);
+            driver.findElement(By.xpath(".//*//li[2]//div[1]//div[3]//ul[1]//li[2]//a[1]")).click();
+            String Mobile2 = driver.findElement(By.xpath("//h2/a[@title='Sony Xperia']")).getText();
+            System.out.println("Mobile2 = " + Mobile2);
+
+            Thread.sleep(2000);
+
+            //Click Compare button
+            driver.findElement(By.xpath("//button[@title='Compare']")).click();
+
+            Thread.sleep(2000);
+
+            //Switch to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            Thread.sleep(2000);
+
+            //Verify the pop-up window and check that the products are reflected in it
+            //
+            //Heading "COMPARE PRODUCTS" with selected products in it.
+
+            String strHead = ("COMPARE PRODUCTS");
+            String compHead = driver.findElement(By.xpath(".//*[@id='top']/body/div[1]/div[1]/h1")).getText();
+            System.out.println("compHead = " + compHead);
+            String popupMobile1 = driver.findElement(By.xpath("//h2/a[@title='IPhone']")).getText();  // text captured is "IPHONE" in uppercase
+            String popupMobile2 = driver.findElement(By.xpath("//h2/a[@title='Sony Xperia']")).getText();  // text captured "SONY XPERIA" in uppercase
+            System.out.println("popupMobile1 = " + popupMobile1);
+            System.out.println("popupMobile2 = " + popupMobile2);
+            //Close the Popup Windows
+            driver.findElement(By.xpath("//button[@title='Close Window']")).click();
+
+            //Switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Quit
+        driver.quit();
+    }
+
+    @Test
+    public void TC05() {
+        int ShoppingCartCount = 1;
+        String firstName = "tran";
+        String lastName = "minh";
+        String email_address = "minh@gmail.com";
+        String password = "minhtran123";
+        String confirmPassword = "minhtran123";
+
+        StringBuffer verificationError = new StringBuffer();
+        //init web driver session
+        System.setProperty("webdriver.chrome.driver", "D:\\Tools\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        try {
+            //Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+
+            RegisterPage registerPage = new RegisterPage(driver);
+
+            //Step 2. Click on my account link
+            registerPage.clickMyAccountLink();
+            //debug
+            Thread.sleep(2000);
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+            //debug
+            Thread.sleep(2000);
+
+            //Step 3. Click Create an Account link and fill New User information excluding the registered Email ID.
+            registerPage.clickCreateAccountLink();
+            //debug
+            Thread.sleep(2000);
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+            //fill info
+            registerPage.enterFirstName(firstName);
+            registerPage.enterLastName(lastName);
+            registerPage.enterEmail(email_address);
+            registerPage.enterPassword(password);
+            registerPage.enterConfirmPassword(confirmPassword);
+
+            //debug
+            Thread.sleep(2000);
+
+            //Step 4. Click Register
+//            driver.findElement(By.xpath("//button[@title='Register']")).click();
+            registerPage.clickRegisterButton();
+            //debug
+            Thread.sleep(2000);
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+            //debug
+            Thread.sleep(2000);
+            // 5. Verify Registration is done. Expected account registration done.
+            String vWelcome = ("WELCOME, " + firstName.toUpperCase() + " " + lastName.toUpperCase() + "!");
+            String tWelcome = driver.findElement(By.xpath(".//*[@id='top']/body/div[1]/div/div[1]/div/p")).getText();
+            System.out.println("tWelcome = " + tWelcome);
+
+            try {
+                Assert.assertEquals(tWelcome, vWelcome);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            //Step 6. Go to TV menu
+            driver.findElement(By.xpath(".//*[@id='nav']/ol/li[2]/a")).click();
+            //debug
+            Thread.sleep(2000);
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //Step 7. Add product in your wish list - use product - LG LCD
+            driver.findElement(By.xpath("//li/a[@class='link-wishlist']")).click();
+            //debug
+            Thread.sleep(2000);
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            // Step 8. Click SHARE WISHLIST
+            driver.findElement(By.xpath("//button[@title='Share Wishlist']")).click();
+            //debug
+            Thread.sleep(2000);
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+
+            //Step 9. In next page enter Email and a message and click SHARE WISHLIST
+            registerPage.enterEmail(email_address);
+            driver.findElement(By.id("message")).clear();
+            driver.findElement(By.id("message")).sendKeys("A really good smart TV ");
+
+            driver.findElement(By.xpath("//button[@title='Share Wishlist']")).click();
+            //debug
+            Thread.sleep(2000);
+
+            // Step 10. Check wishlist is shared. Expected wishlist shared successfully.
+            String vWishList = "Your Wishlist has been shared.";
+            String wishList = driver.findElement(By.xpath(".//*[@id='top']/body/div[1]/div/div[2]/div/div[2]/div/div[1]/ul/li/ul/li/span")).getText();
+            System.out.println("wishList = " + wishList);
+            try {
+                Assert.assertEquals(vWishList, wishList);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //debug
+            Thread.sleep(3000);
+
+            //screen
+            ShoppingCartCount = (ShoppingCartCount + 1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Quit
+        driver.quit();
+    }
+
 }
